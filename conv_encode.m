283b03894b6726f7176c33d47285a4658eb164fc
Martin Yang <daoyuan@student.chalmers.se>
2016-11-12 13:26:28 +0100

    create conv_encode function, subject to change

---
+++ conv_encode.m
@@ -0,0 +1,10 @@
+function [ coded_bits ] = conv_encode( bits,G )
+%CONV_ENCODE Summary of this function goes here
+%   Detailed explanation goes here
+%   use convolution code defined by G to encode bits.
+%   both bits and coded_bits are row vectors
+%   G = [G1;G2;G3], where G1,G2,G3 are the coefficient of the boolean polynomial
+
+
+end
+
