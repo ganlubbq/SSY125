b213b18d6d0b2d32a3f6a80c518be09df22fe69c
sigrunh <sigrunhrafnsd@gmail.com>
2016-11-12 11:11:06 +0100

    defined constellations

--- bits2sym.m
+++ bits2sym.m
@@ -4,8 +4,24 @@ function [ symbols ] = bits2sym( bits,mod_type )
 %   mod_type 1 -> BPSK; 2->QPSK; 3->AMPM
 %   'symbols' is a row vector
 BPSK_const = [-1 1];
-QPSK_const = [];
-AMPM+const = [];
+
+QPSK_const = [...
+            (+1 + 1i)...% 11 - index 1
+            (+1 - 1i)...% 10 - index 2
+            (-1 + 1i)...% 01 - index 3
+            (-1 - 1i)...% 00 - index 4
+            ]/sqrt(2);
+        
+AMPM_const = [...
+            (-3 + 3i)...% 001 - index 1
+            (+3 - 3i)...% 100 - index 2
+            (+3 + 1i)...% 110 - index 3
+            (-3 - 1i)...% 011 - index 4
+            (-1 + 1i)...% 101 - index 5
+            (+1 - 1i)...% 000 - index 6
+            (+1 + 3i)...% 010 - index 7
+            (-1 - 3i)...% 111 - index 8
+            ]/sqrt(10);
 
 switch mod_type
     case 1
