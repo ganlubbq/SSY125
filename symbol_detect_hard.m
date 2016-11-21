db1d1763cbc0c737656267cfef1176fef880b323
Martin Yang <daoyuan@student.chalmers.se>
2016-11-21 14:24:19 +0100

    change the constellation of symbol detection

--- symbol_detect_hard.m
+++ symbol_detect_hard.m
@@ -12,21 +12,21 @@ function [ c_hat ] = symbol_detect_hard( y,mod_type )
 BPSK_const = [-1 1];
 
 QPSK_const = [...
-            (+1 + 1i)...% 11 - index 1
+            (+1 + 1i)...% 00 - index 0
+            (-1 + 1i)...% 01 - index 1
             (+1 - 1i)...% 10 - index 2
-            (-1 + 1i)...% 01 - index 3
-            (-1 - 1i)...% 00 - index 4
+            (-1 - 1i)...% 11 - index 3
             ]/sqrt(2);
         
 AMPM_const = [...
+            (1 + -1i)...% 000 - index 0
             (-3 + 3i)...% 001 - index 1
-            (+3 - 3i)...% 100 - index 2
-            (+3 + 1i)...% 110 - index 3
-            (-3 - 1i)...% 011 - index 4
+            (1 +  3i)...% 010 - index 2
+            (-3 - 1i)...% 011 - index 3
+            (3 - 3i)...% 100 - index 4
             (-1 + 1i)...% 101 - index 5
-            (+1 - 1i)...% 000 - index 6
-            (+1 + 3i)...% 010 - index 7
-            (-1 - 3i)...% 111 - index 8
+            (+3 + 1i)...% 110 - index 6
+            (-1 - 3i)...% 111 - index 7
             ]/sqrt(10);
 
 
