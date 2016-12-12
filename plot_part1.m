c1bee4fdc5652ae995dd9a00d5b741b2fd56a1ee
Martin Yang <daoyuan@student.chalmers.se>
2016-12-12 14:47:06 +0100

    larger simulation number

--- plot_part1.m
+++ plot_part1.m
@@ -1,10 +1,11 @@
 %plot result of part 1
 %% uncoded QPSK vs theoratical
 figure
-semilogy(EbN0,BER)
+semilogy(EbN0,BER,'*')
 hold on
 semilogy(EbN0,BER_th)
 xlabel('EBN0 / dB')
 ylabel('BER')
 grid on
 axis([-1 12 1E-5 1])
+legend('simulation','theoretical')
